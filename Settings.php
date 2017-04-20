33c27659a9aed33862996eadc56c3deca0913e8e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-20 17:03:31 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -2,4 +2,10 @@
 // 2017-04-11
 namespace Dfe\Moip;
 /** @method static Settings s() */
-final class Settings extends \Df\StripeClone\Settings {}
+final class Settings extends \Df\StripeClone\Settings {
+	/**
+	 * 2017-04-20 «The «Token» part of your Test Private Key (Chave de autenticação)»
+	 * @return string
+	 */
+	function privateToken() {return $this->testableP();}
+}
