09434d1044495badf8c1be2317e9aad931a7acfc
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-20 17:14:03 +0300

    no message

---
+++ T/Basic.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\Moip\T;
+// 2017-04-12
+final class Basic extends TestCase {
+	/** @test 2017-04-12 */
+	function t01() {}
+}
---
+++ T/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+namespace Dfe\Moip\T;
+/**
+ * 2017-04-12
+ * @see \Dfe\Moip\T\Basic
+ * @method \Dfe\Moip\Settings s()
+ */
+abstract class TestCase extends \Df\Payment\TestCase {}
