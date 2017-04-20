fe7326cb54ca264df2f4a570f866bed1547515a1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-20 17:30:05 +0300

    no message

--- T/Basic.php
+++ T/Basic.php
@@ -1,9 +1,13 @@
 <?php
 namespace Dfe\Moip\T;
-// 2017-04-12
+use Moip\Moip as API;
+// 2017-04-20
 final class Basic extends TestCase {
-	/** @test 2017-04-12 */
+	/** 2017-04-20 */
 	function t01() {echo df_dump([
 		$this->s()->publicKey(), $this->s()->privateToken(), $this->s()->privateKey()
 	]);}
+
+	/** @test 2017-04-20 */
+	function t02() {echo get_class($this->api());}
 }
--- T/TestCase.php
+++ T/TestCase.php
@@ -1,8 +1,19 @@
 <?php
 namespace Dfe\Moip\T;
+use Moip\Moip as API;
+use Moip\MoipBasicAuth;
 /**
- * 2017-04-12
+ * 2017-04-20
  * @see \Dfe\Moip\T\Basic
  * @method \Dfe\Moip\Settings s()
  */
-abstract class TestCase extends \Df\Payment\TestCase {}
+abstract class TestCase extends \Df\Payment\TestCase {
+	/**
+	 * 2017-04-20
+	 * @return API
+	 */
+	final protected function api() {return dfc($this, function() {$s = $this->s(); return new API(
+		new MoipBasicAuth($s->privateToken(), $s->publicKey())
+		,$this->s()->test() ? API::ENDPOINT_SANDBOX : API::ENDPOINT_PRODUCTION
+	);});}
+}
