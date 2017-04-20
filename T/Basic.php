bb90892b34ee2e87f73e927e3acb84d5e399cec0
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-20 17:17:22 +0300

    no message

--- T/Basic.php
+++ T/Basic.php
@@ -3,5 +3,7 @@ namespace Dfe\Moip\T;
 // 2017-04-12
 final class Basic extends TestCase {
 	/** @test 2017-04-12 */
-	function t01() {}
+	function t01() {echo df_dump([
+		$this->s()->publicKey(), $this->s()->privateToken(), $this->s()->privateKey()
+	]);}
 }
