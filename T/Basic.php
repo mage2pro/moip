01ff37ace845064add06a18496cb9e7054eb5a0c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-22 09:48:08 +0300

    no message

--- T/Basic.php
+++ T/Basic.php
@@ -23,17 +23,36 @@ final class Basic extends TestCase {
 		// https://dev.moip.com.br/reference#criar-um-cliente
 		$c
 			// 2017-04-22
+			// «STUB»
+			// STUB
+			->setBirthDate(DT::createFromFormat('Y-m-d', '1982-07-08'))
+			// 2017-04-22
+			// «Email from the client.»
+			// Required, String (45)
+			->setEmail('admin@mage2.pro')
+			// 2017-04-22
+			// «Full name of customer.»
+			// Required, String (90)
+			->setFullname('Dmitry Fedyuk')
+			// 2017-04-22
 			// «Customer Id. External reference.»
-			// String (66)
+			// Required, String (66)
 			// It should be unique, otherwise you will get the error:
 			// «O identificador prßprio deve ser único, j¹ existe um customer com o identificador informado»
 			// («The unique identifier must be unique, there is a customer with the identified identifier»).
-			->setOwnId(1/*uniqid('df-customer-')*/)
-			->setBirthDate(DT::createFromFormat('Y-m-d', '1982-07-08'))
-			->setFullname('Dmitry Fedyuk')
-			->setEmail('admin@mage2.pro')
-			->setTaxDocument('22222222222', 'CPF')
+			->setOwnId(uniqid('df-customer-'))
+			/**
+			 * 2017-04-22
+			 * «Customer's phone, with country code, area code and number»
+			 * @param int $areaCode
+			 * @param int $number
+			 * @param int $countryCode [optional]
+			 */
 			->setPhone(11, 66778899, 55)
+			// 2017-04-22
+			// «STUB»
+			// STUB				
+			->setTaxDocument('22222222222', 'CPF')
 			->addAddress(
 				C::ADDRESS_SHIPPING, 'Avenida Faria Lima', '2927', 'Itaim'
 				, 'Sao Paulo', 'SP', '01234000', '8'
