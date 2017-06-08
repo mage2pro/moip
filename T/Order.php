fe4272ade0d4dcf4a2ae2e18f0ae02843328918c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-09 01:28:59 +0300

    no message

--- T/Order.php
+++ T/Order.php
@@ -25,6 +25,9 @@ final class Order extends TestCase {
 
 	/**
 	 * 2017-06-08
+	 * «Order values.»
+	 * Required, String(66).
+	 * My notes: the order amount is calculated automatically by Moip.
 	 * @used-by pOrder()
 	 * @return array(string => mixed)
 	 */
@@ -38,18 +41,57 @@ final class Order extends TestCase {
 		// Optional, String.
 		,'subtotals' => [
 			// 2017-06-09
-			// «Addition amount. It will be added to the items amount.»
+			// «Addition amount. It will be added to the items amount. In cents.»
 			// Optional, Integer(12).
 			'addition' => ''
 			// 2017-06-09
-			// «Discount amount. This value will be deducted from the total amount.»
+			// «Discount amount. This value will be deducted from the total amount. In cents.»
 			// Optional, Integer(12).
 			,'discount' => ''
 			// 2017-06-09
-			// «Shipping cost. It will be added to the items amount.»
+			// «Shipping cost. It will be added to the items amount. In cents.»
 			// Optional, Integer(12).
 			,'shipping' => ''
 		]
+	];}
+
+	/**
+	 * 2017-06-08
+	 * «Customer.
+	 * It can be an ID for a customer previously created
+	 * or the collection of attributes to create a new one.»
+	 * @used-by pOrder()
+	 * @return array(string => mixed)
+	 */
+	private function pCustomer() {return [
+		'id' => ''
+	];}
+
+	/**
+	 * 2017-06-08
+	 * @used-by pOrder()
+	 * @return array(string => mixed)
+	 */
+	private function pItem() {return [
+		// 2017-06-09
+		// «Description.»
+		// Optional, String(250).
+		// It is required for the Protected Sales Program:
+		// https://dev.moip.com.br/v2.0/docs/venda-protegida
+		'detail' => ''
+		// 2017-06-09
+		// «Price of 1 product. (The value is multiplied according to the number of products.).
+		// In cents.»
+		// Required, Integer(12).
+		,'price' => ''
+		// 2017-06-09
+		// «Product name.»
+		// Required, String(256).
+		,'product' => ''
+		// 2017-06-09
+		// «Quantity of products.»
+		// Required, Integer(12).
+		,'quantity' => ''
 	];}
 
 	/**
@@ -64,10 +106,14 @@ final class Order extends TestCase {
 		// My notes: the order amount is calculated automatically by Moip.
 		'amount' => $this->pAmount()
 		// 2017-06-09
+		// «Customer.
+		// It can be an ID for a customer previously created
+		// or the collection of attributes to create a new one.»
+		// Required.
+		,'customer' => $this->pCustomer()
+		// 2017-06-09
 		// «Items structure.»
-		,'items' => [
-			
-		]
+		,'items' => [$this->pItem()]
 		// 2017-06-09
 		// «Own id of an order. External reference.»
 		// Required, String(66).
