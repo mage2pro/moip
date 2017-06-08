787bbe1ff38e0879f15c528e6088aecbd36552d6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-09 01:03:22 +0300

    no message

--- T/Order.php
+++ T/Order.php
@@ -30,16 +30,41 @@ final class Order extends TestCase {
 
 	/**
 	 * 2017-06-08
+	 * https://dev.moip.com.br/page/api-reference#section-create-a-customer-post-
+	 * https://dev.moip.com.br/v2.0/reference#criar-um-cliente
 	 * @used-by t01_create()
 	 * @return array(string => mixed)
 	 */
 	private function pOrder() {return [
-		// 2017-06-08
-		// «Customer Id. External reference.»
+		// 2017-06-09
+		// «Order values.»
 		// Required, String(66).
-		// It should be unique, otherwise you will get the error:
-		// «O identificador prßprio deve ser único, j¹ existe um customer com o identificador informado»
-		// («The unique identifier must be unique, there is a customer with the identified identifier»).
-		'ownId' => df_uid(4, 'admin@mage2.pro-')
+		'amount' => [
+			// 2017-06-09
+			// «Currency used in the order. Possible values: BRL. Default value BRL.»
+			// Optional, String.
+			'currency' => 'BRL'
+			// 2017-06-09
+			// «Structure of additional order values.»
+			// Optional, String.
+			,'subtotals' => [
+				// 2017-06-09
+				// «Addition amount. It will be added to the items amount.»
+				// Optional, Integer(12).
+				'addition' => ''
+				// 2017-06-09
+				// «Discount amount. This value will be deducted from the total amount.»
+				// Optional, Integer(12).
+				,'discount' => ''
+				// 2017-06-09
+				// «Shipping cost. It will be added to the items amount.»
+				// Optional, Integer(12).
+				,'shipping' => ''
+			]
+		]
+		// 2017-06-09
+		// «Own id of an order. External reference.»
+		// Required, String(66).
+		,'ownId' => df_uid(4, 'admin@mage2.pro-')
 	];}
 }
