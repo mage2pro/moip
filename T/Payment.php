ac7902529d71d70eb6771da479e5f6e849806e3f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-09 11:25:29 +0300

    no message

--- T/Payment.php
+++ T/Payment.php
@@ -1,5 +1,6 @@
 <?php
 namespace Dfe\Moip\T;
+use Dfe\Moip\SDK\Payment as lP;
 use Moip\Exceptions\UnautorizedException as leUnautorized;
 use Moip\Exceptions\UnexpectedException as leUnexpected;
 use Moip\Exceptions\ValidationException as leValidation;
@@ -7,12 +8,14 @@ use Moip\Exceptions\ValidationException as leValidation;
 // https://dev.moip.com.br/page/api-reference#section-payments
 // https://dev.moip.com.br/v2.0/reference#pagamentos
 final class Payment extends TestCase {
-	/** @test 2017-06-09 */
+	/** 2017-06-09 */
 	function t00() {}
 
-	/** 2017-06-09 */
+	/** @test 2017-06-09 */
 	function t01_create() {
 		try {
+			//echo lP::create('ORD-TKZ1BQOQL69J', $this->pPayment())->j();
+			echo df_json_encode_pretty($this->pPayment());
 		}
 		catch (\Exception $e) {
 			/** @var \Exception|leUnautorized|leUnexpected|leValidation $e */
@@ -20,4 +23,16 @@ final class Payment extends TestCase {
 			throw $e;
 		}
 	}
+
+	/**
+	 * 2017-06-09
+	 * @used-by t01_create()
+	 * @return array(string => mixed)
+	 */
+	private function pPayment() {return [
+		// 2017-06-09
+		// «Identification of your store on the buyer's credit card invoice»
+		// Optional, String(13).
+		'statementDescriptor' => ''
+	];}
 }
