b54227bb7de93df4dcaec7fa139469067e67d474
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-11 09:05:59 +0300

    no message

---
+++ Charge.php
@@ -0,0 +1,54 @@
+<?php
+namespace Dfe\Moip;
+// 2017-06-11
+final class Charge extends \Df\StripeClone\Charge {
+	/**
+	 * 2017-06-11
+	 * @override
+	 * @see \Df\StripeClone\Charge::cardIdPrefix()
+	 * @used-by \Df\StripeClone\Charge::usePreviousCard()
+	 * @return string
+	 */
+	protected function cardIdPrefix() {return null;}
+
+	/**
+	 * 2017-06-11
+	 * @override
+	 * @see \Df\StripeClone\Charge::pCharge()
+	 * @used-by \Df\StripeClone\Charge::request()
+	 * @return array(string => mixed)
+	 */
+	protected function pCharge() {return [];}
+	
+	/**
+	 * 2017-06-11
+	 * @override
+	 * @see \Df\StripeClone\Charge::pCustomer()
+	 * @used-by \Df\StripeClone\Charge::newCard()
+	 * @return array(string => mixed)
+	 */
+	protected function pCustomer() {return [];}
+
+	/**
+	 * 2017-06-11
+	 * Ключ, значением которого является токен банковской карты.
+	 * Этот ключ передаётся как параметр ДВУХ РАЗНЫХ запросов к API ПС:
+	 * 1) в запросе на проведение транзакции (charge)
+	 * 2) в запросе на сохранение банковской карты для будущего повторного использования
+	 * @override
+	 * @see \Df\StripeClone\Charge::k_CardId()
+	 * @used-by \Df\StripeClone\Charge::request()
+	 * @used-by \Df\StripeClone\Charge::newCard()
+	 * @return string
+	 */
+	protected function k_CardId() {return null;}
+
+	/**
+	 * 2017-06-11
+	 * @override
+	 * @see \Df\StripeClone\Charge::k_DSD()
+	 * @used-by \Df\StripeClone\Charge::request()
+	 * @return string
+	 */
+	protected function k_DSD() {return null;}
+}
