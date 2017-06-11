9a9451f5231b8dfcde6e06d472b1a5d9e7a54e66
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-11 09:56:55 +0300

    0.4.3

--- Charge.php
+++ Charge.php
@@ -1,5 +1,6 @@
 <?php
 namespace Dfe\Moip;
+use Dfe\Moip\SDK\Option;
 // 2017-06-11
 final class Charge extends \Df\StripeClone\Charge {
 	/**
@@ -37,8 +38,8 @@ final class Charge extends \Df\StripeClone\Charge {
 	 * 2) в запросе на сохранение банковской карты для будущего повторного использования
 	 * @override
 	 * @see \Df\StripeClone\Charge::k_CardId()
-	 * @used-by \Df\StripeClone\Charge::request()
 	 * @used-by \Df\StripeClone\Charge::newCard()
+	 * @used-by \Df\StripeClone\Charge::request()
 	 * @return string
 	 */
 	protected function k_CardId() {return null;}
@@ -52,4 +53,36 @@ final class Charge extends \Df\StripeClone\Charge {
 	 * @return string
 	 */
 	protected function k_DSD() {return 'statementDescriptor';}
+
+	/**
+	 * 2017-06-11
+	 * @override
+	 * @see \Df\StripeClone\Charge::v_CardId()
+	 * @used-by \Df\StripeClone\Charge::newCard()
+	 * @used-by \Df\StripeClone\Charge::request()
+	 * @param string $id
+	 * @param bool $isPrevious [optional]
+	 * @return array(string => mixed)
+	 */
+	protected function v_CardId($id, $isPrevious = false) {return [
+		// 2017-06-09
+		// «Credit Card data. It can be:
+		// *) the ID of a credit card previously saved,
+		// *) an encrypted credit card hash
+		// *) the whole collection of credit card attributes (in case you have PCI DSS certificate).»
+		// [Moip] The test bank cards: https://mage2.pro/t/3776
+		//
+		// hash: «Encrypted credit card data»
+		// Conditional, String.
+		// https://dev.moip.com.br/v2.0/docs/criptografia
+		//
+		// id: «Credit card ID.
+		// This ID can be used in the future to create new payments. Internal reference.»
+		// Conditional, String(16).
+		'creditCard' => [$isPrevious ? 'id' : 'hash' => $id]
+		// 2017-06-09
+		// «Method used. Possible values: CREDIT_CARD, BOLETO, ONLINE_BANK_DEBIT, WALLET»
+		// Required, String.
+		,'method' => Option::BANK_CARD
+	];}
 }
