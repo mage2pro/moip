5899a9767fd284f2d8f2d23e9bbd2f4e980307eb
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-11 17:46:34 +0300

    0.4.5

--- P/Charge.php
+++ P/Charge.php
@@ -4,53 +4,26 @@ use Dfe\Moip\SDK\Option;
 // 2017-06-11
 final class Charge extends \Df\StripeClone\P\Charge {
 	/**
-	 * 2017-06-11
-	 * @override
-	 * @see \Df\StripeClone\P\Charge::cardIdPrefix()
-	 * @used-by \Df\StripeClone\P\Charge::usePreviousCard()
-	 * @return string
-	 */
-	protected function cardIdPrefix() {return null;}
-
-	/**
-	 * 2017-06-11
-	 * @override
-	 * @see \Df\StripeClone\P\Charge::p()
-	 * @used-by \Df\StripeClone\P\Charge::request()
-	 * @return array(string => mixed)
-	 */
-	protected function p() {return [];}
-
-	/**
 	 * 2017-06-11 Ключ, значением которого является токен банковской карты.
+	 * https://github.com/mage2pro/moip/blob/0.4.4/T/CaseT/Payment.php#L46-L49
 	 * @override
 	 * @see \Df\StripeClone\P\Charge::k_CardId()
 	 * @used-by \Df\StripeClone\P\Charge::request()
 	 * @return string
 	 */
-	protected function k_CardId() {return null;}
-
-	/**
-	 * 2017-06-11
-	 * https://github.com/mage2pro/moip/blob/0.4.2/T/CaseT/Payment.php#L50-L53
-	 * @override
-	 * @see \Df\StripeClone\P\Charge::k_DSD()
-	 * @used-by \Df\StripeClone\P\Charge::request()
-	 * @return string
-	 */
-	protected function k_DSD() {return 'statementDescriptor';}
+	function k_CardId() {return 'fundingInstrument';}
 
 	/**
 	 * 2017-06-11
 	 * @override
 	 * @see \Df\StripeClone\P\Charge::v_CardId()
-	 * @used-by \Df\StripeClone\P\Charge::newCard()
+	 * @used-by \Dfe\Moip\P\Reg::v_CardId()
 	 * @used-by \Df\StripeClone\P\Charge::request()
 	 * @param string $id
 	 * @param bool $isPrevious [optional]
 	 * @return array(string => mixed)
 	 */
-	protected function v_CardId($id, $isPrevious = false) {return [
+	function v_CardId($id, $isPrevious = false) {return [
 		// 2017-06-09
 		// «Credit Card data. It can be:
 		// *) the ID of a credit card previously saved,
@@ -71,4 +44,32 @@ final class Charge extends \Df\StripeClone\P\Charge {
 		// Required, String.
 		,'method' => Option::BANK_CARD
 	];}
+
+	/**
+	 * 2017-06-11
+	 * @override
+	 * @see \Df\StripeClone\P\Charge::cardIdPrefix()
+	 * @used-by \Df\StripeClone\P\Charge::usePreviousCard()
+	 * @return string
+	 */
+	protected function cardIdPrefix() {return null;}
+
+	/**
+	 * 2017-06-11
+	 * @override
+	 * @see \Df\StripeClone\P\Charge::p()
+	 * @used-by \Df\StripeClone\P\Charge::request()
+	 * @return array(string => mixed)
+	 */
+	protected function p() {return [];}
+
+	/**
+	 * 2017-06-11
+	 * https://github.com/mage2pro/moip/blob/0.4.2/T/CaseT/Payment.php#L50-L53
+	 * @override
+	 * @see \Df\StripeClone\P\Charge::k_DSD()
+	 * @used-by \Df\StripeClone\P\Charge::request()
+	 * @return string
+	 */
+	protected function k_DSD() {return 'statementDescriptor';}
 }
--- P/Reg.php
+++ P/Reg.php
@@ -1,16 +1,32 @@
 <?php
 namespace Dfe\Moip\P;
 // 2017-06-11
+/** @method Charge charge() */
 final class Reg extends \Df\StripeClone\P\Reg {
 	/**
+	 * 2017-06-10
+	 * This option is undocumented in the Portuguese documentation:
+	 * But it is mentioned in the English documentation:
+	 * https://dev.moip.com.br/page/api-reference#section-create-a-customer-post-
 	 * 2017-06-11
 	 * Ключ, значением которого является токен банковской карты.
 	 * Этот ключ передаётся как параметр в запросе на сохранение банковской карты
 	 * для будущего повторного использования при регистрации нового покупателя.
+	 * https://github.com/mage2pro/moip/blob/0.4.4/T/CaseT/Customer.php#L106-L110
 	 * @override
 	 * @see \Df\StripeClone\P\Reg::k_CardId()
 	 * @used-by \Df\StripeClone\P\Reg::request()
 	 * @return string
 	 */
-	protected function k_CardId() {return null;}
+	protected function k_CardId() {return 'fundingInstruments';}
+
+	/**
+	 * 2017-06-11
+	 * @override
+	 * @see \Df\StripeClone\P\Reg::v_CardId()
+	 * @used-by \Df\StripeClone\P\Reg::request()
+	 * @param string $id
+	 * @return array(array(string => mixed))
+	 */
+	protected function v_CardId($id) {return [$this->charge()->v_CardId($id)];}
 }
