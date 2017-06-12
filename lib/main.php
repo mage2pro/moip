701a486cf5d7db29271d4e57657b32759047cb5e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-12 11:02:37 +0300

    no message

--- lib/main.php
+++ lib/main.php
@@ -8,11 +8,8 @@
 function dfe_moip_phone($s) {return dfcf(function($s) {/** @var array(string => string)|null $a */return
 	!($a = df_phone_explode([$s, 'BR'], false)) || 3 > count($a) || 55 !== intval($a[0]) ? [] :
 		array_combine([
-			// 2017-04-25 «Your phone's local code (DDD)», Integer(2).
-			'areaCode'
-			// 2017-04-25 «ID number of the phone. Possible values: 55.», Integer(2).
-			,'countryCode'
-			// 2017-04-25 «Telephone number.», Integer(9).
-			,'number'
+			'areaCode' // 2017-04-25 «Your phone's local code (DDD)», Integer(2).
+			,'countryCode' // 2017-04-25 «ID number of the phone. Possible values: 55.», Integer(2).
+			,'number' // 2017-04-25 «Telephone number.», Integer(9).
 		], $a)
 ;}, [$s]);}
