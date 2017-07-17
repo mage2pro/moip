535788ddff00792a3f2a6297672e91aba019f225
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-17 03:18:56 +0300

    0.7.3

---
+++ Facade/O.php
@@ -0,0 +1,17 @@
+<?php
+namespace Dfe\Moip\Facade;
+use Df\API\Operation;
+// 2017-07-17
+/** @method \Dfe\Paymill\Method m() */
+final class O extends \Df\StripeClone\Facade\O {
+	/**
+	 * 2017-07-17
+	 * `[Moip] An example of a response to «POST v2/orders/<order ID>/payments»` https://mage2.pro/t/4048
+	 * @override
+	 * @see \Df\StripeClone\Facade\O::toArray()
+	 * @used-by \Df\StripeClone\Method::transInfo()
+	 * @param Operation $o
+	 * @return array(string => mixed)
+	 */
+	function toArray($o) {return $o->res();}
+}
