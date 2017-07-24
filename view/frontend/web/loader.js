df0896570e16d03a54e44eacfbd9d432b2c359dd
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-24 12:06:54 +0300

    no message

--- view/frontend/web/loader.js
+++ view/frontend/web/loader.js
@@ -1,1 +1,11 @@
-define(['Df_Payment/loader'], function(l) {return l('Dfe_Moip');});
+// 2017-07-24
+define([
+	'df', 'Magento_Checkout/js/model/payment/renderer-list', 'uiComponent'
+], function(df, rendererList, Component) {'use strict';
+/** @type {String} */ var code = 'dfe_moip';
+/** @type {String} */ var name = 'Dfe_Moip';
+if (window.checkoutConfig.payment[code]) {
+	rendererList.push({type: code, component: name + '/main'});
+}
+return Component.extend({});
+});
