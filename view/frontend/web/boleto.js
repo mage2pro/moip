cb5e67bc880179eecf4eec37b7e07cb22ef5618c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-24 15:24:16 +0300

    no message

---
+++ view/frontend/web/boleto.js
@@ -0,0 +1,2 @@
+// 2017-07-24
+define(['Df_Payment/custom'], function(parent) {'use strict'; return parent.extend({});});
--- view/frontend/web/loader.js
+++ view/frontend/web/loader.js
@@ -5,6 +5,9 @@ define([
 /** @type {String} */ var code = 'dfe_moip';
 /** @type {String} */ var name = 'Dfe_Moip';
 if (window.checkoutConfig.payment[code]) {
+	// 2017-07-24
+	// `rendererList` is an «observable array»: http://knockoutjs.com/documentation/observableArrays.html
+	// https://github.com/magento/magento2/blob/2.2.0-RC1.5/app/code/Magento/Checkout/view/frontend/web/js/model/payment/renderer-list.js#L11
 	rendererList.push({type: code, component: name + '/card'});
 }
 return Component.extend({});
