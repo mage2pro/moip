5de90921854be80c33a696b4eac1d4713bf2fd11
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-04 14:59:30 +0300

    1.0.4

--- CardFormatter.php
+++ CardFormatter.php
@@ -1,7 +1,6 @@
 <?php
 namespace Dfe\Moip;
 use Df\Payment\BankCardNetworks as N;
-use Df\Sales\Plugin\Model\Order\Email\Sender\OrderSender as OS;
 use Dfe\Moip\Facade\Card as C;
 // 2017-07-19
 /** @method C c() */
@@ -16,7 +15,7 @@ final class CardFormatter extends \Df\StripeClone\CardFormatter {
 	 */
 	function label() {$c = $this->c(); /** @var C $c */return
 		df_pad($c->first6(), $c->numberLength() - 4, '·') . "{$c->last4()} " .
-			(OS::is() ? "({$c->brand()})" :
+			(df_sales_email_sending() ? "({$c->brand()})" :
 				df_tag('img', N::dimensions(null, 20) + ['alt' => $c->brand(), 'src' => N::url($c->logoId())])
 			)
 	;}
