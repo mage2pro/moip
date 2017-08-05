d8ff295865ba9ee41c390b2ef1fb5e223c27aa59
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-05 19:02:40 +0300

    1.0.5

--- Block/Info/Boleto.php
+++ Block/Info/Boleto.php
@@ -16,16 +16,41 @@ class Boleto extends \Df\Payment\Block\Info {
 	 * @used-by \Dfe\Moip\Block\Info\Card::prepare()
 	 */
 	final protected function prepare() {
-		$res0 = $this->tm()->res0(); /** @var array(string => mixed) */
-		$url = $res0['_links']['payBoleto']['redirectHref']; /** @var string $url */
 		if ($this->extended()) {
+			$res0 = $this->tm()->res0(); /** @var array(string => mixed) */
 			$this->si([
 				'Payment Option' => 'Boleto bancário'
-				,'Payment Slip' => df_tag_ab($res0['id'], $url)
+				,'Payment Slip' => df_tag_ab($res0['id'], $this->url(false))
 			]);
 		}
 		else {
-			$this->si(null, df_tag_ab(__('Print the boleto'), "$url/print"));
+			$this->si(null, df_tag_ab(__('Print the boleto'), $this->url()));
 		}
+	}
+
+	/**
+	 * 2017-08-05
+	 * @override
+	 * @see \Df\Payment\Block\Info::rCustomerAccount()
+	 * @used-by \Df\Payment\Block\Info::_toHtml()
+	 * @return string
+	 */
+	protected function rCustomerAccount() {return df_tag('div', 'df-payment-info moip boleto',
+		df_block_output($this, 'boleto', [
+			'title' => $this->m()->getTitle()
+			,'url' => $this->url()
+		])
+	);}
+
+	/**
+	 * 2017-08-05
+	 * @used-by prepare()
+	 * @used-by rCustomerAccount()
+	 * @param bool $print [optional]
+	 * @return string
+	 */
+	private function url($print = true) {
+		$r = $this->tm()->res0('_links/payBoleto/redirectHref'); /** @var string $r */
+		return !$print ? $r : "$r/print";
 	}
 }
