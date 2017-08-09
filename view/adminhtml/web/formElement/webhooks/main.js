f38356a7649cdb113ad47882858d35f77c3088ab
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-09 23:25:59 +0300

    no message

--- view/adminhtml/web/formElement/webhooks/main.js
+++ view/adminhtml/web/formElement/webhooks/main.js
@@ -1,5 +1,5 @@
 // 2017-08-09
-define(['df-lodash', 'jquery', 'domReady!'], function(_, $) {return (
+define(['df', 'df-lodash', 'jquery', 'domReady!'], function(df, _, $) {return (
 	/**
 	 * 2017-08-09
 	 * @param {Object} config
@@ -9,8 +9,12 @@ define(['df-lodash', 'jquery', 'domReady!'], function(_, $) {return (
 	function(config) {
 		/** @type {jQuery} HTMLDivElement */ var $c = $(document.getElementById(config.id));
 		var $ul = $('<ul/>');
-		_.each(config.urls, function(v){
-			$ul.append($('<li>').html(v));
+		/** @type {jQuery} HTMLDivElement */
+		var $delete = $('<div>').addClass('df-delete fa fa-trash-o').click(function() {
+			console.log('delete');
+		});
+		_.each(config.urls, function(v) {
+			$ul.append($('<li>').html(v).append($delete.clone(true)));
 		});
 		$c.append($ul);
 	}
