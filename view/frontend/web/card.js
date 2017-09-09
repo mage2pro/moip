ecbef116a873007644e6d5dc0ea08ff25eee764f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-09 19:32:33 +0300

    1.0.20

--- view/frontend/web/card.js
+++ view/frontend/web/card.js
@@ -7,7 +7,14 @@ define([
 ], function(mixin, df, _, dfc, baChange, parent, ko, quote) {'use strict';
 /** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */
 return parent.extend(df.o.merge(mixin, {
-	defaults: {df: {card: {requireCardholder: true}, moip: {suffix: 'card'}}, dob: '', taxID: ''},
+	defaults: {
+		df: {
+			card: {requireCardholder: true}, moip: {suffix: 'card'}
+			/** 2017-09-09 @used-by Df_Payment/mixin::dfFormCssClasses() */
+			,placeOrderButtonAlignment: 'center'
+		}
+		, dob: '', taxID: ''
+	},
 	/**
 	 * 2017-07-14
 	 * @override
