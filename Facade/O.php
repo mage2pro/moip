9bb7286418f23745de35db4444218a68e90d5631
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-06 19:26:41 +0000

    refactoring

--- Facade/O.php
+++ Facade/O.php
@@ -5,8 +5,8 @@ use Df\API\Operation;
 /** @method \Dfe\Paymill\Method m() */
 final class O extends \Df\StripeClone\Facade\O {
 	/**
-	 * 2017-07-17
-	 * `[Moip] An example of a response to «POST v2/orders/<order ID>/payments»` https://mage2.pro/t/4048
+	 * 2017-07-17 `[Moip] An example of a response to «POST v2/orders/<order ID>/payments»` https://mage2.pro/t/4048
+	 * 2023-01-06 We can not declare the argument's type because it is undeclared in the overriden method.
 	 * @override
 	 * @see \Df\StripeClone\Facade\O::toArray()
 	 * @used-by \Df\StripeClone\Method::transInfo()
