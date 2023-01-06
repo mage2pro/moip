06963ded87154147d9fb92cc821450ff1f9dd4a3
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-06 19:13:13 +0000

    https://github.com/mage2pro/core/issues/168

--- Settings.php
+++ Settings.php
@@ -34,7 +34,6 @@ final class Settings extends \Df\StripeClone\Settings {
 
 	/**
 	 * 2017-04-20 «The «Token» part of your Test Private Key (Chave de autenticação)»
-	 * @return string
 	 */
-	function privateToken() {return $this->testableP();}
+	function privateToken():string {return $this->testableP();}
 }
